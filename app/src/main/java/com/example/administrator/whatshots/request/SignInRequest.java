b3c38560bf6b8dd78db790713328bac50065e029
zym <392951475@qq.com>
2017-11-15 10:20:41 +0800

    add appkey and shopdomain

---
+++ app/src/main/java/com/example/administrator/whatshots/request/SignInRequest.java
@@ -0,0 +1,11 @@
+package com.example.administrator.whatshots.request;
+
+import com.shopify.buy3.Storefront;
+
+/**
+ * Created by Administrator on 2017/11/15.
+ */
+
+public class SignInRequest {
+
+}
---
+++ app/src/main/java/com/example/administrator/whatshots/request/SingUpRequest.java
@@ -0,0 +1,8 @@
+package com.example.administrator.whatshots.request;
+
+/**
+ * Created by Administrator on 2017/11/15.
+ */
+
+public class SingUpRequest {
+}

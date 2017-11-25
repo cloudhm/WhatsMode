444204e40056700705ff282ca72a3657bc3a1e3a
tom <tom.chen@whatsmode.com>
2017-11-25 10:44:43 +0800

    splash

--- app/src/main/java/com/whatsmode/shopify/block/account/SplashActivity.java
+++ app/src/main/java/com/whatsmode/shopify/block/account/SplashActivity.java
@@ -1,15 +1,23 @@
 package com.whatsmode.shopify.block.account;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.view.WindowManager;
 
 import com.whatsmode.shopify.AppNavigator;
 import com.whatsmode.shopify.R;
 import com.whatsmode.shopify.block.account.data.AccountManager;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Created by tom on 17-11-23.
@@ -23,7 +31,6 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-
         getWindow().getDecorView().postDelayed(() -> splash(),1000);
     }
 
@@ -34,6 +41,6 @@ public class SplashActivity extends AppCompatActivity {
         }else{
             AppNavigator.jumpToMain(this);
         }
-        finish();
     }
+
 }

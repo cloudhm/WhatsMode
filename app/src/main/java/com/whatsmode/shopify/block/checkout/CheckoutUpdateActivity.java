5ad921fc77a851dc5a1e3c93eb430bfb8ab3ecf9
zym <392951475@qq.com>
2017-11-29 10:39:41 +0800

    checkout item

--- app/src/main/java/com/whatsmode/shopify/block/checkout/CheckoutUpdateActivity.java
+++ app/src/main/java/com/whatsmode/shopify/block/checkout/CheckoutUpdateActivity.java
@@ -2,15 +2,22 @@ package com.whatsmode.shopify.block.checkout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.shopify.graphql.support.ID;
 import com.whatsmode.library.util.ListUtils;
+import com.whatsmode.library.util.PreferencesHelper;
+import com.whatsmode.library.util.PreferencesUtil;
+import com.whatsmode.library.util.ScreenUtils;
 import com.whatsmode.library.util.ToastUtil;
 import com.whatsmode.shopify.AppNavigator;
 import com.whatsmode.shopify.R;
@@ -19,8 +26,12 @@ import com.whatsmode.shopify.block.account.data.AccountManager;
 import com.whatsmode.shopify.block.address.Address;
 import com.whatsmode.shopify.block.cart.CartItem;
 import com.whatsmode.shopify.block.cart.CartItemLists;
+import com.whatsmode.shopify.common.Constant;
 import com.whatsmode.shopify.mvp.MvpActivity;
 import com.whatsmode.shopify.ui.helper.ToolbarHelper;
+
+import java.io.IOException;
+import java.util.List;
 
 public class CheckoutUpdateActivity extends MvpActivity<CheckoutUpdateContact.Presenter> implements CheckoutUpdateContact.View, View.OnClickListener {
 
@@ -57,17 +68,34 @@ public class CheckoutUpdateActivity extends MvpActivity<CheckoutUpdateContact.Pr
     private void addItemToLayout() {
         if(dataSource == null || ListUtils.isEmpty(dataSource.cartItems))
             return;
+        TextView tvTitle = new TextView(this);
+        try {
+            List<CartItem> cartItems = (List<CartItem>) PreferencesUtil.getObject(this, Constant.CART_LOCAL);
+            tvTitle.setText("Product(" + (ListUtils.isEmpty(cartItems) ? 0 : cartItems.size()) + ")");
+            tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP,18);
+            tvTitle.setPadding(ScreenUtils.dip2px(this,15),ScreenUtils.dip2px(this,15),ScreenUtils.dip2px(this,15),0);
+            tvTitle.setTextColor(Color.BLACK);
+            layoutContainer.addView(tvTitle);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         for (CartItem i : dataSource.cartItems) {
-            View view = LayoutInflater.from(this).inflate(R.layout.item_cart, null);
+            View view = LayoutInflater.from(this).inflate(R.layout.checkout_item_cart, null);
             TextView tvName = (TextView) view.findViewById(R.id.description);
             tvName.setText(i.getName());
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            Glide.with(this).load(i.getIcon())
+                    .asBitmap()
+                    .centerCrop()
+                    .into(icon);
             TextView tvPrice = (TextView) view.findViewById(R.id.price);
-            tvPrice.setVisibility(View.VISIBLE);
-            tvPrice.setText(String.valueOf(i.getPrice()));
-            TextView tvQuality = (TextView) view.findViewById(R.id.tvQuality);
-            tvQuality.setVisibility(View.VISIBLE);
-            tvQuality.setText(String.valueOf(i.getQuality()));
-            view.findViewById(R.id.operation_layout).setVisibility(View.GONE);
+            TextView tvFontSize = (TextView) view.findViewById(R.id.sizeAndColor);
+            tvFontSize.setText(i.getSize());
+            tvPrice.setText(new StringBuilder(String.valueOf(i.getPrice())).append("USD"));
+            TextView tvCount = (TextView) view.findViewById(R.id.checkout_count);
+            tvCount.setText(new StringBuilder("x").append(String.valueOf(i.getQuality())));
             layoutContainer.addView(view);
         }
     }

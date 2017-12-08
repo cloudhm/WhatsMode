79f745341b73e0b9ab0b79dac255c801ba05e144
tom <tom.chen@whatsmode.com>
2017-12-08 20:19:11 +0800

    all

--- app/src/main/java/com/whatsmode/shopify/block/me/ShareUtil.java
+++ app/src/main/java/com/whatsmode/shopify/block/me/ShareUtil.java
@@ -28,7 +28,7 @@ public class ShareUtil {
         // text是分享文本，所有平台都需要这个字段
         oks.setText(text);
         //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        oks.setImageUrl("");//"http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg"
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         oks.setImagePath(imagePath);//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
